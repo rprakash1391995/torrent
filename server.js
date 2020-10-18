03bdcc17d2f451f8b60e09363a798396c25795d9
Ravi Prakash <rprakash1391995@gmail.com>
2020-10-18 19:22:25 +0530

    fixed

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) =>{
+    res.render("index");
+});
+var port = process.env.PORT || 3000;
+app.listen(port, () =>{
+    console.log("app running");
+})
